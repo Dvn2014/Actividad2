8bdde97f34e642b5d826e735ba5c41a8f4e10fd1
Dvn2014 <85833322+Dvn2014@users.noreply.github.com>
2022-05-28 01:49:49 -0500

    Unidad 2. AA3. Actividad de avance 1 Caso práctico a partir de una historia de usuario}

---
+++ database/migrations/2022_05_26_121745_categories.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Categories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',120);
+            $table->text('description',250);     
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
